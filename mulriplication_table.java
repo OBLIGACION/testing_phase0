2e0e54e47dcffb23ab5738daef74cfda19053500
OBLIGACION <cherupio369@gmail.com>
2023-10-15 17:22:06 +0800

    first beanch

---
+++ mulriplication_table.java
@@ -0,0 +1,17 @@
+// MULTIPLICATION TABLE
+public class Main {
+	public static void main(String[] args) {
+		
+		int table = 10;
+		int max = 10;
+		int n,i;
+		
+		for(n=1;n<=table;n++){
+		    for(i=1;i<=max;i++){
+		        System.out.println(n +" x "+ i +" = "+ n*i);
+		        } 
+		        System.out.println("=========");
+		    }
+		
+	}
+}
